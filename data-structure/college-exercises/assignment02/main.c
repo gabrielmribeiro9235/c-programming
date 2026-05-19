eda5ff7a9ca2fabd070c47eede9bda1a7d15e0de
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-05-19 20:04:55 -0300

    feat: create carregarDados function

--- data-structure/college-exercises/assignment02/main.c
+++ data-structure/college-exercises/assignment02/main.c
@@ -14,10 +14,31 @@ typedef struct {
 } Turma;
 
 void inicializarTurma(Turma *turma);
+void carregarDados(Turma *turma);
 
 void inicializarTurma(Turma *turma) {
     turma->dados = NULL;
     turma->quantidade = 0;
 
     printf("Turma criada e pronta para receber alunos!\n");
+}
+
+void carregarDados(Turma *turma) {
+    FILE *arq = fopen("dados.bin", "rb");
+
+    if(arq == NULL)
+        return;
+    
+    fread(&turma->quantidade, sizeof(int), 1, arq);
+
+    if(turma->quantidade > 0) {
+        turma->dados = (Aluno *)malloc(sizeof(Aluno) * turma->quantidade);
+
+        if(turma->dados == NULL)
+            exit(1);
+
+        fread(turma->dados, sizeof(Aluno), turma->quantidade, arq);
+    }
+
+    fclose(arq);
 }
