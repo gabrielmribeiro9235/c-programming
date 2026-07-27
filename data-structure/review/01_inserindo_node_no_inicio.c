5b1dd94fa725de04a9d1238bc2a60150a7a153e2
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-07-27 13:26:51 -0300

    refactor: improve algorithm to add a node in the start of a node list

--- data-structure/review/01_inserindo_node_no_inicio.c
+++ data-structure/review/01_inserindo_node_no_inicio.c
@@ -8,46 +8,46 @@ typedef struct No {
 
 void insere_inicio(No **cabeca, int valor);
 void teste_de_impressao(No **cabeca);
+void libera_memoria(No **cabeca);
 
 int main() {
     No *lista = NULL;
     int valor1 = 10, valor2 = 20, valor3 = 30;
-
+    
     insere_inicio(&lista, valor1);
     insere_inicio(&lista, valor2);
     insere_inicio(&lista, valor3);
-
+    
     teste_de_impressao(&lista);
-
-    free(lista);
-    lista = NULL;
+    
+    libera_memoria(&lista);
+    
     return 0;
 }
 
 void insere_inicio(No **cabeca, int valor) {
-    if(*cabeca == NULL) {
-        *cabeca = malloc(sizeof(No));
-        if(*cabeca == NULL) return;
-        (*cabeca)->valor = valor;
-        (*cabeca)->proximo = NULL;
-    } else {
-        No *deslocamento = malloc(sizeof(No));
-        deslocamento->valor = (*cabeca)->valor;
-        deslocamento->proximo = (*cabeca)->proximo;
-        (*cabeca)->valor = valor;
-        (*cabeca)->proximo = deslocamento;
-    }
+    No *novo = malloc(sizeof(No));
     
+    if(novo == NULL) return;
+    
+    novo->valor = valor;
+    novo->proximo = *cabeca;
+    *cabeca = novo;
 }
 
 void teste_de_impressao(No **cabeca) {
     if(*cabeca == NULL) return;
-
-    if((*cabeca)->proximo == NULL) {
-        printf("%d\t", (*cabeca)->valor);
-        return;
-    }
-
+    
     printf("%d\t", (*cabeca)->valor);
     teste_de_impressao(&(*cabeca)->proximo);
+}
+
+void libera_memoria(No **cabeca) {
+    No *atual = *cabeca;
+    while (atual != NULL) {
+        No *proximo = atual->proximo;
+        free(atual);
+        atual = proximo;
+    }
+    *cabeca = NULL;    
 }
